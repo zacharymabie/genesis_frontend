21f18f5744d97f8abf0524d40727add8fbea2de5
Zach Mabie <zzmabie@yahoo.com>
2022-08-17 00:03:45 +0800

    FeedContainer.js and posts.json

--- screens/feed/FeedContainer.js
+++ screens/feed/FeedContainer.js
@@ -1,1 +1,51 @@
-console.log("damn goblin mode");
+import React, { useState, useEffect, useCallback } from "react";
+import {
+  View,
+  StyleSheet,
+  ActivityIndicator,
+  ScrollView,
+  Dimensions,
+  Text,
+  Image
+} from "react-native";
+
+import PostContainer from "./PostContainer";
+const data = require("../../assets/data/posts.json")
+
+var { width } = Dimensions.get('window')
+
+const FeedContainer = () => {
+    const [posts, setPost] = useState(data);
+
+    return (
+    <ScrollView>
+        <View style={styles.container}>
+            {posts.map(post => {
+                    return <PostContainer 
+                        key={post.id}
+                        name={post.author.name}
+                        profilePhoto={post.author.profilePic}
+                        timestamp={post.timestamp}
+                        caption={post.caption}
+                        imagePost={post.image}
+                        likes={post.likes}
+                        comments={post.comments}
+                    />
+            })}
+        </View>
+    </ScrollView>
+    );
+};
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      backgroundColor: 'gainsboro',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: width,
+  }
+});
+
+export default FeedContainer;
+
