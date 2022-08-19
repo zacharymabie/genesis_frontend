4ef096d138214f0aa306b0c8a98fa6568557f5f7
Zach Mabie <zzmabie@yahoo.com>
2022-08-19 16:36:31 +0800

    bonk

--- App.js
+++ App.js
@@ -6,11 +6,12 @@ import Register from "./screens/profile/Register";
 import ProgramContainer from "./screens/programs/ProgramsContainer";
 import FeedContainer from "./screens/feed/FeedContainer";
 import ProfileContainer from "./screens/profile/ProfileContainer"
+import NewPost from "./screens/feed/NewPost";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <ProfileContainer />
+      <NewPost />
     </SafeAreaView>
   );
 }
